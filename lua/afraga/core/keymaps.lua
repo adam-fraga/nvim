9a863f29c43a489cbe3bb109f0c4eaed527b5d14
Adam FRG <adam.fraga@hotmail.com>
2024-11-14 22:39:13 -0500

    Add php lsp server config, more file for tailwind lsp support and change keymap for buffer resizing

--- lua/afraga/core/keymaps.lua
+++ lua/afraga/core/keymaps.lua
@@ -1,6 +1,13 @@
-vim.api.nvim_set_keymap("n", "<C-Up>", ":resize +2<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-Down>", ":resize -2<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+-- RESIZE OLD
+-- vim.api.nvim_set_keymap("n", "<C-Up>", ":resize +2<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-Down>", ":resize -2<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap("n", "<Up>", ":resize +2<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Down>", ":resize -2<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
 vim.api.nvim_set_keymap("i", "kj", "<Esc>", { noremap = true })
--- lua/afraga/plugins/lsp/lspconfig.lua
+++ lua/afraga/plugins/lsp/lspconfig.lua
@@ -102,11 +102,20 @@ return {
 			on_attach = on_attach,
 		})
 
+    -- configure php server
+    lspconfig["intelephense"].setup({
+      cmd ={ "intelephense", "--stdio" },
+			capabilities = capabilities,
+			on_attach = on_attach,
+			filetypes = {"php", "blade"},
+      format = {true}
+    })
+
 		-- configure tailwindcss server
 		lspconfig["tailwindcss"].setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
-			filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+			filetypes = { "aspnetcorerazor", "astro", "astro-markdown", "blade", "clojure", "django-html", "htmldjango", "edge", "eelixir", "elixir", "ejs", "erb", "eruby", "gohtml", "gohtmltmpl", "haml", "handlebars", "hbs", "html", "htmlangular", "html-eex", "heex", "jade", "leaf", "liquid", "markdown", "mdx", "mustache", "njk", "nunjucks", "php", "razor", "slim", "twig", "css", "less", "postcss", "sass", "scss", "stylus", "sugarss", "javascript", "javascriptreact", "reason", "rescript", "typescript", "typescriptreact", "vue", "svelte", "templ" },
 			init_options = { userLanguages = { templ = "html" } },
 		})
 
